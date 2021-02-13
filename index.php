3fafa4e67d0a7850719fa00cdc59be25902376d6
ArseneMacmaag <skyanggara123@gmail.com>
2021-02-13 11:55:55 +0700

    Menambahkan paragraph

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat inventore provident voluptates mollitia iste debitis architecto quas eveniet explicabo libero unde laboriosam ullam ea praesentium, ipsum eos? Ipsum, nisi ad maxime minus, voluptatum porro impedit sapiente numquam ullam perferendis corporis tempora! Consequuntur ullam magni deserunt impedit, repudiandae, quidem, deleniti doloribus eligendi necessitatibus facere ipsum. Beatae et animi voluptates expedita labore! Itaque iusto laboriosam iste quo alias facere consectetur dignissimos dolore soluta vitae? Sequi explicabo inventore modi magni quis nam reiciendis porro, similique tenetur doloremque? Perferendis dicta praesentium sapiente ut eveniet inventore sint, earum sed porro at eum dolorem quisquam error?</p>
 </body>
 
 </html>
